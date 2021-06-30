8ecfb5923d7be8f804b27450ad4098bd4afcf1c4
Muhammad Nabeel <m.nabeeel96@gmail.com>
2021-06-30 20:26:41 +0500

    'build'

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+
 function Avatar({ url, className }) {
   return (
-    <img
+    <Image
       loading='lazy'
+      width={40}
+      height={40}
       className={` h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       src={url}
       alt='Profile pic'
--- pages/index.js
+++ pages/index.js
@@ -56,13 +56,13 @@ export default function Home() {
           height={100}
         />
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
-          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <SearchIcon className='h-5 mr-3 text-gray-500 cursor-pointer' />
           <input
             ref={searchInputRef}
             type='text'
             className=' flex-grow focus:outline-none'
           />
-          <MicrophoneIcon className='h-5' />
+          <MicrophoneIcon className='h-5 cursor-pointer' />
         </div>
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
           <button onClick={search} className='btn'>
