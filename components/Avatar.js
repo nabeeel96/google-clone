f59d32ed6ea3755ab13d64459038cf74a0911691
Muhammad Nabeel <m.nabeeel96@gmail.com>
2021-06-30 21:21:44 +0500

    'build'

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,14 +1,14 @@
-function Avatar({ url, className }) {
-  return (
-    <Image
-      loading='lazy'
-      className={
-        ' h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
-      }
-      src={url}
-      alt='Profile pic'
-    />
-  );
-}
+// function Avatar({ url, className }) {
+//   return (
+//     <Image
+//       loading='lazy'
+//       className={
+//         ' h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110'
+//       }
+//       src={url}
+//       alt='Profile pic'
+//     />
+//   );
+// }
 
-export default Avatar;
+// export default Avatar;
