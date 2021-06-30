e685bff376a56c2904fdb7a24349ea5f416bad4a
Muhammad Nabeel <m.nabeeel96@gmail.com>
2021-06-30 20:38:56 +0500

    'img'

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,7 @@
-import Image from 'next/image';
-
 function Avatar({ url, className }) {
   return (
-    <Image
+    <img
       loading='lazy'
-      width={40}
-      height={40}
       className={` h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
       src={url}
       alt='Profile pic'
--- components/Footer.js
+++ components/Footer.js
@@ -7,8 +7,10 @@ function Footer() {
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3 '>
         <div className='flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2'>
-          <GlobeIcon className='h-5 mr-1 text-green-700 ' /> Carbon neutral
-          since 2007
+          <GlobeIcon className='h-5 mr-1 text-green-700 ' />{' '}
+          <p>
+            Made by <a href='mailto:m.nabeeel96@gmail.com'>Muhammad Nabeel</a>
+          </p>
         </div>
         <div className='flex justify-center space-x-8 whitespace-nowrap md:justify-self-start'>
           <p>Advertising</p>
